1c0972c1912636b4346d4842a10898197582d631
冯建军 <1500542712@qq.com>
2022-04-08 00:18:48 +0800

    feat: 搞定微应用基本样式

--- src/components/layout/micro-bar.vue
+++ src/components/layout/micro-bar.vue
@@ -1,15 +1,47 @@
 <template>
   <section class="micro-menu-list">
-    <div class="micro-item">
+    <div
+      v-for="item in microList"
+      :key="item.key"
+      :style="item.color ? '--micro-color: ' + item.color : ''"
+      class="micro-item"
+    >
       <div class="micro-item-color"></div>
+      <i class="icon-font" :class="item.icon || 'icon-m-moren'"></i>
     </div>
   </section>
 </template>
 <script lang="ts">
-  import { defineComponent } from 'vue';
+  import { defineComponent, reactive } from 'vue';
+  type MicroItem = {
+    // 微应用项
+    name: string;
+    icon?: string;
+    key: string;
+    color: string;
+  };
   export default defineComponent({
     setup() {
-      console.log('测试');
+      // 获取微应用菜单
+      let microList: Array<MicroItem> = reactive([]);
+      microList = [
+        {
+          name: '管理页面',
+          // icon: 'icon-admin',
+          key: 'admin',
+          color: '#2D55F6',
+        },
+        {
+          name: '权限管理',
+          icon: 'icon-quanxian',
+          key: 'auth',
+          color: '#FF4D4F',
+        },
+      ];
+
+      return {
+        microList,
+      };
     },
   });
 </script>
@@ -17,20 +49,44 @@
   .micro-menu-list {
     display: flex;
     align-items: center;
+    margin: 0 -5px;
     .micro-item {
       width: 48px;
       height: 48px;
       background: #ffffff;
-      box-shadow: 4px 0px 24px 0px rgba(0, 0, 0, 0.08);
+      box-shadow: 4px 0px 50px 0px rgba(0, 0, 0, 0.18);
       border-radius: 8px;
       position: relative;
+      overflow: hidden;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin: 0 5px;
+      &:hover {
+        .micro-item-color {
+          transform: scale(1);
+        }
+        .icon-font {
+          font-size: 30px;
+        }
+      }
       .micro-item-color {
         position: absolute;
         left: 0;
         right: 0;
         top: 0;
         bottom: 0;
-        background: #ff4d4f;
+        background-color: var(--micro-color, #13c2c2);
+        border-radius: 8px;
+        transform: scale(0.5);
+        transition: all 0.5s ease-in-out;
+      }
+      .icon-font {
+        color: #ffffff;
+        transition: font-size 0.45s ease-out 0.3s;
+        font-size: 16px;
+        position: relative;
+        z-index: 99;
       }
     }
   }
--- src/components/login-page.vue
+++ src/components/login-page.vue
@@ -5,88 +5,88 @@
 </template>
 <script lang="ts"></script>
 <style lang="scss" scoped>
-@property --perA {
-  syntax: "<percentage>";
-  inherits: false;
-  initial-value: 75%;
-}
-
-@property --perB {
-  syntax: "<percentage>";
-  inherits: false;
-  initial-value: 99%;
-}
-
-@property --perC {
-  syntax: "<percentage>";
-  inherits: false;
-  initial-value: 15%;
-}
-
-@property --perD {
-  syntax: "<percentage>";
-  inherits: false;
-  initial-value: 16%;
-}
-
-@property --perE {
-  syntax: "<percentage>";
-  inherits: false;
-  initial-value: 86%;
-}
-
-@property --angle {
-  syntax: "<angle>";
-  inherits: false;
-  initial-value: 0deg;
-}
-@keyframes move {
-  100% {
-    --perA: 85%;
-    --perB: 49%;
-    --perC: 45%;
-    --perD: 39%;
-    --perE: 70%;
-    --angle: 360deg;
+  @property --perA {
+    syntax: '<percentage>';
+    inherits: false;
+    initial-value: 75%;
   }
-}
-.fixed-filter-model {
-  position: fixed;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  animation: move 30s infinite alternate linear;
-  background-image: radial-gradient(
-      circle at var(--perE) 7%,
-      rgba(#7824fe, 0.2) 0%,
-      rgba(#7824fe, 0.2) 50%,
-      rgba(#4248ff, 0.5) 50%,
-      rgba(#4248ff, 0.5) 100%
-    ),
-    radial-gradient(
-      circle at var(--perC) var(--perD),
-      rgba(#fdaccc, 0.2) 0%,
-      rgba(#fdaccc, 0.2) 50%,
-      rgba(#8361ff, 0.2) 50%,
-      rgba(#8361ff, 0.04) 100%
-    ),
-    radial-gradient(
-      circle at var(--perA) var(--perB),
-      rgba(#671dff, 0.2) 0%,
-      rgba(#671dff, 0.04) 50%,
-      rgba(#8e2bff, 0.2) 50%,
-      rgba(#8e2bff, 0.04) 100%
-    );
-  &::before {
-    content: "";
-    position: absolute;
+
+  @property --perB {
+    syntax: '<percentage>';
+    inherits: false;
+    initial-value: 99%;
+  }
+
+  @property --perC {
+    syntax: '<percentage>';
+    inherits: false;
+    initial-value: 15%;
+  }
+
+  @property --perD {
+    syntax: '<percentage>';
+    inherits: false;
+    initial-value: 16%;
+  }
+
+  @property --perE {
+    syntax: '<percentage>';
+    inherits: false;
+    initial-value: 86%;
+  }
+
+  @property --angle {
+    syntax: '<angle>';
+    inherits: false;
+    initial-value: 0deg;
+  }
+  @keyframes move {
+    100% {
+      --perA: 85%;
+      --perB: 49%;
+      --perC: 45%;
+      --perD: 39%;
+      --perE: 70%;
+      --angle: 360deg;
+    }
+  }
+  .fixed-filter-model {
+    position: fixed;
     top: 0;
     left: 0;
     bottom: 0;
     right: 0;
-    backdrop-filter: blur(130px);
-    z-index: 0;
+    animation: move 30s infinite alternate linear;
+    background-image: radial-gradient(
+        circle at var(--perE) 7%,
+        rgba(#7824fe, 0.2) 0%,
+        rgba(#7824fe, 0.2) 50%,
+        rgba(#4248ff, 0.5) 50%,
+        rgba(#4248ff, 0.5) 100%
+      ),
+      radial-gradient(
+        circle at var(--perC) var(--perD),
+        rgba(#fdaccc, 0.2) 0%,
+        rgba(#fdaccc, 0.2) 50%,
+        rgba(#8361ff, 0.2) 50%,
+        rgba(#8361ff, 0.04) 100%
+      ),
+      radial-gradient(
+        circle at var(--perA) var(--perB),
+        rgba(#671dff, 0.2) 0%,
+        rgba(#671dff, 0.04) 50%,
+        rgba(#8e2bff, 0.2) 50%,
+        rgba(#8e2bff, 0.04) 100%
+      );
+    &::before {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      backdrop-filter: blur(130px);
+      z-index: 0;
+    }
   }
-}
 </style>
