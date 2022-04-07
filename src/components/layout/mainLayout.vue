0d19faa853d5051eb366702c2ffff0e1a1a335a8
冯建军 <1500542712@qq.com>
2022-04-07 22:10:26 +0800

    feat: 格式化部分组件

--- src/components/layout/mainLayout.vue
+++ src/components/layout/mainLayout.vue
@@ -4,19 +4,19 @@
   </section>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue';
-import NavBar from './nav-bar.vue';
-export default defineComponent({
-  components: { NavBar },
-  setup() {
-    console.log('sdfdsf');
-  },
-});
+  import { defineComponent } from 'vue';
+  import NavBar from './nav-bar.vue';
+  export default defineComponent({
+    components: { NavBar },
+    setup() {
+      console.log('sdfdsf');
+    },
+  });
 </script>
 <style lang="scss" scoped>
-.layout-main-wrap {
-  width: 100%;
-  height: 100%;
-  // display: flex;
-}
+  .layout-main-wrap {
+    width: 100%;
+    height: 100%;
+    // display: flex;
+  }
 </style>
